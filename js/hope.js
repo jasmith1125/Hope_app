d40da6e70192a8e2c50377d1f73dc683a83007f0
jasmith1125 <jasmith1125@yahoo.com>
2015-05-05 20:11:29 -0400

    select boxes

--- js/hope.js
+++ js/hope.js
@@ -5,7 +5,7 @@ window.onload = function() {
 
 // Got help from: http://stackoverflow.com/questions/13330202/how-to-create-list-of-checkboxes-dynamically-
 // with-javascript
-
+/*
 function populate(select1, select2) {
 	var s1 = document.getElementById(select1);
 	var s2 = document.getElementById(select2);
@@ -51,6 +51,45 @@ function populate(select1, select2) {
 $(document).on('change', 'input[type=checkbox]', function(e) {
     alert(s2.value);
 });
+*/
+
+// Function called when the first menu's value changes.
+// Function updates the second menu.
+// Create array of pet arrays; array index of each nested array
+// corresponds to index of the array elements in primaryMenu (html)
+var serv = [
+    [''],
+    ['Service Type', 'Car repairs', 'Pro Bono Legal Help', 'Landscaping', 'Tree Trimming', 'Plumbing Service', 'Electrical Service', 'Carpentry'],
+    ['Service Type','Resident Tutor', 'Resident Mentor', 'Resident Event Planner'],
+    ['Service Type', 'Donation Pickup', 'Clean Vacant Unit', 'Paint Vacant Unit', 'Stock Vacant Unit']
+];
+
+// Create secondary menu of pet breeds/alternate types
+function createServMenu () {
+    // Get the index from the primaryMenu selection
+    var selection = this.selectedIndex
+    // Get the secondary menu and assign to a variable
+    var list = document.getElementById("servMenu");
+    // Remove elements from secondary (pet breed) menu
+    list.innerHTML = '';
+    
+    // For the selected index, add OPTION elements
+    for (var i = 0; i < serv[selection].length; i++) {
+        var newOpt = document.createElement("OPTION");
+        // Create text node to hold the array items
+        var text  = document.createTextNode(serv[selection][i]);
+        // Append text node to option element
+        newOpt.appendChild(text);
+        // Append option element to the secondary select menu
+        list.appendChild(newOpt);
+    }        
+}
+// Add an event handler to generate secondary menu when a selection
+// is made in the primary menu
+document.getElementById("primaryMenu").onchange = createServMenu;
+
+// End select box exercise
+
 
 
 
